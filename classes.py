bff66484f1375602af3881c631004d35d85544b6
Ricardo J. Rodríguez <ricardo@sekhmet.unizar.es>
2021-02-16 20:26:25 +0100

    added new class descriptors

--- classes.py
+++ classes.py
@@ -1,10 +1,15 @@
 
 CLASS_LIST = """
 Edificio Ada Byron:
-
+  - AB07            Aula 0.07
+  - ABSalonActos    Salón de Actos
+  - ABLabL0.03      Laboratorio DIIS L0.03
+  - ABLabL0.04      Laboratorio DIIS L0.04
+  
 Edificio Torres Quevedo:
 
 Edificio Betancourt:
-  - BT0_02    Aula 0.02
-  - BTJuntas  Sala de Juntas
+  - BT0_02          Aula 0.02
+  - BT1_04          Aula 1.04
+  - BTJuntas        Sala de Juntas
 """
